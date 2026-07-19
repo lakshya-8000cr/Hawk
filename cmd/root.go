9d4fbcf8e12ae185a73d170f5ce568f773964bdc
lakshya-8000cr <lakshyachauhan147@gmail.com>
2026-07-19 15:43:05 +0530

    architeture

--- cmd/root.go
+++ cmd/root.go
@@ -7,11 +7,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var Version = "v1.0.0-dev"
+
 var rootCmd = &cobra.Command{
 	Use:   "kubectl-hawk",
 	Short: "Dependency-aware Kubernetes impact analysis",
 	Long: `Hawk analyzes relationships between Kubernetes resources
 before potentially destructive operations are performed.`,
+}
+
+
+//to show the version 
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print Hawk version",
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Hawk %s\n", Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
 }
 
 func Execute() {
